5e53de11563cea2080543477aed6771c7eb5d549
Atia Khan <atia.khan@xloopdigital.com>
2023-05-31 11:56:28 +0500

    user model

---
+++ src/main/java/com/accountservices/users/UserRepository.java
@@ -0,0 +1,9 @@
+package com.accountservices.users;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+}
