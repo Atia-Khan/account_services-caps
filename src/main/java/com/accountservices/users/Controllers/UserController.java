3a252c5374d55d81cb721bce85de6eda4c27f19b
Atia Khan <atia.khan@xloopdigital.com>
2023-06-05 13:14:46 +0500

    User Services test cases completed.

--- src/main/java/com/accountservices/users/Controllers/UserController.java
+++ src/main/java/com/accountservices/users/Controllers/UserController.java
@@ -1,55 +1,38 @@
 package com.accountservices.users.Controllers;
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.accountservices.users.Model.User;
 import com.accountservices.users.Repositories.UserRepository;
-
 @RestController
 @RequestMapping("/user")
 public class UserController {
-    
     @Autowired
     private UserRepository userRepo;
-
     @GetMapping("")
     public List<User> getAllUsers(){
         return userRepo.findAll();
     }
-
     @PostMapping("/signup")
     public void post_User(@RequestBody User user){
         this.userRepo.save(user);
     }
-
+    
+  
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody User user) {
-        // Optional<User> optionalUser = userRepo.findById(id);
-        // User user = optionalUser.get();
-        Long id=user.getUserId();
-        Optional<User> optional=userRepo.findById(id);
-
-        User userDb = 
-
-        if(user.getEmail().equals(user) && user.getPassword().equals(user)){
-            return ResponseEntity.ok("Login successful");
+    public void login(@RequestBody User user) {
+        Optional<User> optional = userRepo.findByEmail(user.getEmail());
+        if (optional.isPresent()) {
+            User userDb = optional.get();
+            if (userDb.getPassword().equals(user.getPassword())) {
+                System.out.println("Login successful");
+            }
         }
-        
-        else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User not found");
-        }
-        
-    }    
+    }
 }
 
--- src/main/java/com/accountservices/users/Repositories/UserRepository.java
+++ src/main/java/com/accountservices/users/Repositories/UserRepository.java
@@ -1,7 +1,8 @@
 package com.accountservices.users.Repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.accountservices.users.Model.User;
@@ -9,6 +10,6 @@ import com.accountservices.users.Model.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-   
-
+    Optional<User> findByEmail(String email);
+    
 }
